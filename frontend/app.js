4a007f9639c565df02fd013be0b7d5691feb3909
Vishwesh-tech <vishweshrai.2004.ff@gmail.com>
2025-09-11 02:57:07 +0530

    All task updated

---
+++ frontend/app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const axios = require("axios");
+const bodyParser = require("body-parser");
+const path = require("path"); 
+
+const URL=process.env.BACKEND_URL || "http://localhost:8150/submit";
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.post("/submit", async (req, res) => {
+  const { name, surname , email , message} = req.body;
+  try {
+    const response = await axios.post(URL, { name, surname, email, message });
+    res.send(response.data);
+  } catch (error) {
+    console.error("Error details:", error.message);
+    res.status(500).send("Error connecting to backend");
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Frontend running on port 3000");
+});
