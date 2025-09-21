a1f6d3e384ad64c1f288baa72c91918e4da695eb
Vishwesh Rai <vishweshrai.2004.ff@gmail.com>
2025-09-22 01:54:28 +0530

    Update app.py

--- flask-app/app.py
+++ flask-app/app.py
@@ -38,4 +38,4 @@ def submit():
         return jsonify({"error": "Method not allowed"}), 405
 if __name__ == '__main__':
 
-    app.run(port=3000,host='0.0.0.0', debug=True)
+    app.run(port=5000,host='0.0.0.0', debug=True)
